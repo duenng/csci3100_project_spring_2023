9db2970e672ebd37f53c0c9c2e4ef36563cfa7da
duenng <103260610+duenng@users.noreply.github.com>
2023-04-07 03:28:31 +0800

    update

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 3001;
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./firebase-service-account.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const mongoURI = 'mongodb+srv://duenng:t5l5q29t@csci3100.unxmyiq.mongodb.net/test';
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Error connecting to MongoDB:', err);
+});
+
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
