42a09af8b94f3858872527c800a6b43bdd5666a4
duenng <103260610+duenng@users.noreply.github.com>
2023-04-07 01:30:33 +0800

    update

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Listening on port ${port}`));
