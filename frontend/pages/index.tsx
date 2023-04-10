bc8559e8700c5cf042b646aa75ccdde81293cd6e
duenng <103260610+duenng@users.noreply.github.com>
2023-04-11 01:54:37 +0800

    update

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Sidebar from '@/components/Sidebar'
+import Login from '@/components/Login'
+import Feed from "@/components/Feed";
+import Trending from "@/components/Trending";
+import Suggestions from "@/components/Suggestions";
+import { auth } from './firebase';
+import {AuthContextProvider} from '@/components/UserContext';
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="main-content flex">
+        <Sidebar />
+        <Feed />
+        <div className="right-section">
+          <Trending />
+		  <AuthContextProvider>
+		  <Login />
+		  </AuthContextProvider>
+          <Suggestions />
+        </div>
+      </div>
+    </div>
+  );
+}
