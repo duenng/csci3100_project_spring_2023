d74e881213e2a32bf8487c3cb5c5ab4a84a412df
TommyS725 <120657256+TommyS725@users.noreply.github.com>
2023-04-10 17:11:04 +0800

    ajusted index style, basic outline of post

--- frontend/components/post.jsx
+++ frontend/components/post.jsx
@@ -1,24 +1,48 @@
-import { DotsHorizontalIcon} from "@heroicons/react/outline";
+import { DotsHorizontalIcon, ArrowLeftIcon} from "@heroicons/react/outline";
+import { Content } from "next/font/google";
 
+// shd use post id to fetch
+let username = "test"
+let tag = "@test"
+let text = "here is the testing content."
+const date = new Date(2023, 3, 10, 13, 0, 20);
 export default function Post(){
 
-    return(
-        <>
-        <div  className=" flex items-center justify-left text-black-700 justify-start-onlarge" >
-        {/* User Profile Picture, Need to import from database*/}
-        <img
-            src="/usericon.jpg"
-            alt="Profile Picture"
-            className=" w-12 h-12 rounded-full"
-        />
+      const hour = date.getHours() % 12;
+      const minute = date.getMinutes();
+      const year = date.getFullYear();
+      const month = date.toLocaleString('default', { month: 'long' });
+      const day = date.getDate();
+      const period = date.getHours() < 12 ? 'am' : 'pm';
+      const formattedDate = `${period} ${hour}:${minute.toString().padStart(2, '0')} · ${year} ${month} ${day}`;
 
-        <div className="">
-        {/* User Name, Need to import from database*/}
-          <h4 style={{padding:0, margin:0}}>Username</h4>
-          <p style={{padding:0, margin:0}}>@username</p>
-        </div>
-        <DotsHorizontalIcon className="h-5 ml-auto"/>
-      </div>
-        </> 
+
+    return(
+       <>
+            <div className="flex text-2xl h-16 align-middle  bg-opacity-25 sticky top-0 backdrop-blur-sm">
+              <ArrowLeftIcon className="m-4"/>
+              <a className="my-4 mx-1">Posts</a>
+            </div>
+            <div className="my-4 flex items-center justify-left text-black-700 justify-start-onlarge mx-3">
+              {/* User Profile Picture, Need to import from database*/}
+              <img src="/usericon.jpg" alt="Profile Picture" className="w-12 h-12 rounded-full px-1"/>
+              <div className="px-1 text-m">
+                {/* User Name, Need to import from database*/}
+                <h4 style={{ padding: 0, margin: 0 }}>{username}</h4>
+                <p style={{ padding: 0, margin: 0 }} className="text-gray-500">
+                  {tag}
+                </p>
+              </div>
+              <DotsHorizontalIcon className="h-5 ml-auto" />
+            </div>
+            <div className="text-l my-4 mx-3">
+              {text}
+            </div>
+            <div className=" text-gray-500 text-sm my-4 mx-3">
+              {formattedDate}
+            </div>
+            
+         
+      </> 
     )
 }
--- frontend/pages/post/index.js
+++ frontend/pages/post/index.js
@@ -6,25 +6,28 @@ import Sidebar from '@/components/Sidebar'
 import Trending from "@/components/Trending";
 import Suggestions from "@/components/Suggestions";
 import Post from "@/components/Post"
+import PostHeader from '../../components/postHeader'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className="container">
-     
-      <div className="main-content flex">
-      <div className='left-section'>
-      <Sidebar />
+
+    <div className='grid grid-cols-4 gap-2 h-screen w-screen'>
+      <div className=' overflow-y-scroll'>
+        <Sidebar/>
       </div>
-      <div className='w-2/3'>
-        <Post/>
-      </div>
-        <div className="right-section">
-          <Trending />
-          <Suggestions />
+
+      <div className='col-span-2 border-x-4 '>
+        <div className='overflow-y-scroll '>
+          <Post/>
         </div>
       </div>
+      <div className=' overflow-y-scroll	'>
+        <Trending/>
+        <Suggestions/>
+      </div>
+
     </div>
   );
 }
