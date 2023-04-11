d4ead4794d1c6a6f9a22cd52cb793928711fdc72
TommyS725 <120657256+TommyS725@users.noreply.github.com>
2023-04-12 06:32:48 +0800

    Add share and comment focus, fixed redering video in commnt

--- frontend/components/PostCommentSection.jsx
+++ frontend/components/PostCommentSection.jsx
@@ -1,81 +1,38 @@
-import { Icon } from '@iconify/react';
-import {useState} from "react"
+import {  useRef, useState} from "react"
 import Comment from "./Comment";
 import CommentPanel from"./CommentPanel";
+import PostLike from "./postLike";
 
 export default function PostCommentSection(props){
 
-    const [repost,setRepost] = useState(props.repost)
-    const [like,setLike] = useState(props.like)
-    const [liked,setLiked] = useState(like.includes(props.current))
     const [comment,setComment] = useState(props.comment)
+    const commentRef = useRef()
 
-
-      const showCommentPanel = (e)=>{
-        //todo
-          console.log("show panal",e)
+    const handleShowAddComment = ()=>{
+      if (commentRef.current) {
+        let area = commentRef.current.children[0].children[1].children[1]
+        //console.log(area)
+        commentRef.current.scrollIntoView({ behavior: 'smooth' });
+        setTimeout(() => {
+          area.focus()
+        },500);
       }
+    }
 
-      const handleRepost = (e)=>{
-        //todo
-        console.log("reposted",e)
-      }
-
-      const handleUnlike=(e)=>{
-        setLiked(false)
-        if(like.includes(props.current)){
-          let filtered = like.filter(id=>id!==props.current)
-          setLike([...filtered])
-          //fetch
-        }
-        
-      }
-
-      const handleLike=(e)=>{
-        setLiked(true);
-        //console.log(like,props.current)
-        if(!like.includes(props.current)){
-            //console.log("here")
-          setLike(prev=>{
-            return [...prev,props.current]})
-          //fetch
-        }
-      }
-
-      const handleShare=(e)=>{
-        //todo
-        console.log("share",e)
-      }
-
-      const addHandler=(newComment)=>{
-        console.log(2,newComment)
-        setComment(prev=>{
+    const addHandler=(newComment)=>{
+      console.log(2,newComment)
+      setComment(prev=>{
             return [...prev,newComment]
-        })
-      }
+      })
+    }
 
 
     return(
         <>
-          {/* likes and etc... */}
-          <hr className="mx-2"></hr>
-            <div className=" my-4 mx-3 text-m">
-            {repost.length?<span className="mx-2"><a className="font-semibold">{repost.length}</a><a className="text-gray-500"> reposts</a></span>:null}
-            {like.length?<span className="mx-2"><a className="font-semibold">{like.length}</a><a className="text-gray-500"> likes</a></span>:null}
-            </div>
-            <hr className="mx-2"></hr>
-            <div className="flex justify-evenly my-2">
-            <Icon icon="ic:outline-insert-comment" className="hoverEffect h-10" onClick={(e)=>showCommentPanel(e)} width="36" hFlip={true} />
-            <Icon icon="zondicons:repost" className="hoverEffect h-10" onClick={(e)=>handleRepost(e)} width="36"/>
-            {
-              liked?<Icon icon="mdi:cards-heart"  className="hoverEffect h-10" onClick={(e)=>handleUnlike(e)} color="red" height="36" hFlip={true} />:
-              <Icon icon="mdi:cards-heart-outline" className="hoverEffect h-10" onClick={(e)=>handleLike(e)} width="36"/>
-            }
-            <Icon icon="material-symbols:ios-share" className="hoverEffect h-10" onClick={(e)=>handleShare(e)} height="36" hFlip={true} />
-            </div>
+            <PostLike current={props.current} repost={props.repost} like={props.like} handler={()=>handleShowAddComment()}/>
             <hr className="mx-2"></hr>
             {/* comment section */}
-            <div className="mx -2">
+            <div className="mx-2" ref={commentRef}>
               <CommentPanel tag={props.tag} user = {{}} inPost={true} handler={addHandler}/>
             </div>
             <hr className=" mx-2"/>
@@ -84,6 +41,7 @@ export default function PostCommentSection(props){
                 return <Comment key={index} com={com} current={props.current}/>
               })
             }
+            {/* <div className='fiexed w-screen h-screen z-9999 bg-slate-600 top-0 left-0'></div> */}
         </>
     )
 }
