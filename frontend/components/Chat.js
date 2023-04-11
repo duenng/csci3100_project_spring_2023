ea9a7eaa87ca0d44359b6574fe288afc275f5d7e
RickyLi <126090839+RickyLWL@users.noreply.github.com>
2023-04-12 02:57:51 +0800

    Add new Chat.js

---
+++ frontend/components/Chat.js
@@ -0,0 +1,158 @@
+import Image from "next/image"
+import SideBarMenuItem from "./SideBarMenuItem";
+import { HomeIcon } from "@heroicons/react/solid";
+import {
+  BellIcon,
+  InboxIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+} from "@heroicons/react/outline";
+
+function Chat() {
+
+return (
+ 
+<div className="flex">
+
+    <div className="w-30 h-screen bg-gray-100">
+
+        <div className="text-[20px] p-3  font-bold">Chats</div>
+        <div className="p-3 flex">
+            <input 
+                className="p-2 w-10/12 bg-gray-200 text-xs focus:outline-none rounded-tl-md rounded-bl-md"
+                type="text"
+                placeholder="Search for users..."/>
+            <div className="w-2/12 flex justify-center items-center bg-gray-200 rounded-tr-md rounded-br-md">
+                <img className="w-4" src="./search.png" />
+            </div>
+        </div>
+
+        <div className="flex m-3">
+            <div className="p-2 flex justify-center items-center flex-col">
+                <img className="w-10 h-10 rounded-full" src="./usericon.jpg"/>
+                <div className="text-gary-500 text-xs pt-1 text-center">Username</div>
+            </div>
+            <div className="p-2 flex justify-center items-center flex-col">
+                <img className="w-10 h-10 rounded-full" src="./usericon.jpg"/>
+                <div className="text-gary-500 text-xs pt-1 text-center">Username</div>
+            </div>
+            <div className="p-2 flex justify-center items-center flex-col">
+                <img className="w-10 h-10 rounded-full" src="./usericon.jpg"/>
+                <div className="text-gary-500 text-xs pt-1 text-center">Username</div>
+            </div>
+            <div className="p-2 flex justify-center items-center flex-col">
+                <img className="w-10 h-10 rounded-full" src="./usericon.jpg"/>
+                <div className="text-gary-500 text-xs pt-1 text-center">Username</div>
+            </div>
+        </div>
+        <div>
+            <div className="flex m-3 bg-white rounded-lg p-2">
+                <div>
+                    <img className="w-14 h-14 rounded full" src="./usericon.jpg"/>
+                </div>
+                <div className="flex-grow p-3">
+                    <div className="flex text-xs justify-between">
+                        <div>Username</div>
+                        <div className="text-gray-400">12:00 AM</div>
+                    </div>
+                    <div className="text-xs text-gray-400">This is the latest message.</div>
+                </div>
+            </div>
+            <div className="flex m-3 bg-white rounded-lg p-2">
+                <div>
+                    <img className="w-14 h-14 rounded full" src="./usericon.jpg"/>
+                </div>
+                <div className="flex-grow p-3">
+                    <div className="flex text-xs justify-between">
+                        <div>Username</div>
+                        <div className="text-gray-400">12:00 AM</div>
+                    </div>
+                    <div className="text-xs text-gray-400">This is the latest message.</div>
+                </div>
+            </div>
+            <div className="flex m-3 bg-white rounded-lg p-2">
+                <div>
+                    <img className="w-14 h-14 rounded full" src="./usericon.jpg"/>
+                </div>
+                <div className="flex-grow p-3">
+                    <div className="flex text-xs justify-between">
+                        <div>Username</div>
+                        <div className="text-gray-400">12:00 AM</div>
+                    </div>
+                    <div className="text-xs text-gray-400">This is the latest message.</div>
+                </div>
+            </div>
+            <div className="flex m-3 bg-white rounded-lg p-2">
+                <div>
+                    <img className="w-14 h-14 rounded full" src="./usericon.jpg"/>
+                </div>
+                <div className="flex-grow p-3">
+                    <div className="flex text-xs justify-between">
+                        <div>Username</div>
+                        <div className="text-gray-400">12:00 AM</div>
+                    </div>
+                    <div className="text-xs text-gray-400">This is the latest message.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div className="flex-grow h-screen flex flex-col">
+        <div className=" w-full h-30 flex justify-between bg-green-100">
+            <div className="flex items-center">
+                <div className="p-3">
+                    <img className="w-14 h-14 rounded-full" src="./usericon.jpg" />
+                </div> 
+                <div className="p-3">
+                    <div className="text-sm">Username</div>
+                    <div className="text-xs">online</div>    
+                </div> 
+            </div>
+        </div>
+
+        <div className=" w-full h-screen flex-glow bg-blue-100">
+            <div className="flex items-end w-3/6 bg-gray-100 m-8 rounded-lg">
+                <img className="w-10 h-10s rounded-full m-3" src="./usericon.jpg" alt="" />
+                <div className="p-3">
+                    <div className="text-sm" >
+                        Username
+                    </div>
+                    <div className="text-xs text-gray-500">
+                        SUCK MY DICK DUEN!
+                    </div>
+                    <div className="text-xs text-gray-400"> 8 minutes ago</div>
+                </div>           
+            </div>
+
+            <div className="flex justify-end">
+                <div className="flex justify-between w-3/6 bg-blue-600 m-8 rounded-lg">
+                    
+                    <div className="p-5">
+                        <div className="text-xs text-gray-100">Oh Yeah! Come on!</div>
+                        <div className=" flex items-end text-xs text-gray-200"> 8 minutes ago</div>
+                    </div>     
+                    <div className="">
+                    <img className="w-10 h-10 rounded-full m-4" src="./usericon.jpg" alt="" />
+                    </div> 
+                </div>
+            </div>
+
+        </div>
+        <div className=" w-full h-14 flex px-3 bg-red-100">
+            <input placeholder="Start a new message..." type="text" className="flex-grow focus:outline-none "/>
+            <div className =" rounded-full w-5 h-5 flex justify-between">
+                <img src="./sent.png" class="w-5 h-5 m-2"/>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+
+);
+}
+
+export default Chat;
--- frontend/pages/chat/index.tsx
+++ frontend/pages/chat/index.tsx
@@ -3,29 +3,29 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Sidebar from '@/components/Sidebar'
+import Login from '@/components/Login'
 import Feed from "@/components/Feed";
 import Trending from "@/components/Trending";
 import Suggestions from "@/components/Suggestions";
 import {AuthContextProvider} from '@/components/UserContext';
-import Chat from "@/components/Chat";
 import Chatlist from "@/components/Chatlist";
 import Chatroom from "@/components/Chatroom";
+import Chat from "@/components/Chat";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className="container">
-      <div className="main-content flex">
-        <Sidebar />
-        <Feed />
-        <div className="right-section"> 
-        <Trending />
-		  <AuthContextProvider>
-		  </AuthContextProvider>
-          <Suggestions />
-        </div>
+    <div className=" h-screen">
+    <div className='grid grid-cols-5 w-screen h-screen'>
+      <div className=' col-span-1 h-screen'>
+        <Sidebar/>
       </div>
+
+      <div className='col-span-4 h-screen '>
+        <Chat/>
+      </div>
+    </div>
     </div>
   );
 }
