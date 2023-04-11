5a16100211f34c10794dd8045c1eacc6bec80759
duenng <103260610+duenng@users.noreply.github.com>
2023-04-11 16:05:58 +0800

    update index

--- frontend/pages/login/index.tsx
+++ frontend/pages/login/index.tsx
@@ -1,63 +1,41 @@
-import React from "react";
-import styles from "./login.module.css";
+import React, { useState } from "react";
+import styles from "../../styles/login.module.css";
+
 const Login = () => {
+  const [activeForm, setActiveForm] = useState("login");
+
+  const handleFormSwitch = (form) => {
+    setActiveForm(form);
+  };
+
   return (
-    <section className="container">
-      <h1 className="section-title">Animated Forms</h1>
-      <div className="forms">
-        <div className="form-wrapper is-active">
-          <button type="button" className="switcher switcher-login">
+    <section className={styles.container}>
+      <h1 className={styles.sectionTitle}>Authentication</h1>
+      <div className={styles.forms}>
+        <div className={`${styles.formWrapper}${activeForm === "login" ? ` ${styles.isActive}` : ""}`}>
+          <button
+            type="button"
+            className={`${styles.switcher} ${styles.switcherLogin}`}
+            onClick={() => handleFormSwitch("login")}
+          >
             Login
-            <span className="underline"></span>
+            <span className={styles.underline}></span>
           </button>
-          <form className="form form-login">
-            <fieldset>
-              <legend>Please, enter your email and password for login.</legend>
-              <div className="input-block">
-                <label htmlFor="login-email">E-mail</label>
-                <input id="login-email" type="email" required />
-              </div>
-              <div className="input-block">
-                <label htmlFor="login-password">Password</label>
-                <input id="login-password" type="password" required />
-              </div>
-            </fieldset>
-            <button type="submit" className="btn-login">
-              Login
-            </button>
+          <form className={`${styles.form} ${styles.formLogin}`}>
+            {/* ... */}
           </form>
         </div>
-        <div className="form-wrapper">
-          <button type="button" className="switcher switcher-signup">
+        <div className={`${styles.formWrapper}${activeForm === "signup" ? ` ${styles.isActive}` : ""}`}>
+          <button
+            type="button"
+            className={`${styles.switcher} ${styles.switcherSignup}`}
+            onClick={() => handleFormSwitch("signup")}
+          >
             Sign Up
-            <span className="underline"></span>
+            <span className={styles.underline}></span>
           </button>
-          <form className="form form-signup">
-            <fieldset>
-              <legend>
-                Please, enter your email, password and password confirmation for
-                sign up.
-              </legend>
-              <div className="input-block">
-                <label htmlFor="signup-email">E-mail</label>
-                <input id="signup-email" type="email" required />
-              </div>
-              <div className="input-block">
-                <label htmlFor="signup-password">Password</label>
-                <input id="signup-password" type="password" required />
-              </div>
-              <div className="input-block">
-                <label htmlFor="signup-password-confirm">Confirm password</label>
-                <input
-                  id="signup-password-confirm"
-                  type="password"
-                  required
-                />
-              </div>
-            </fieldset>
-            <button type="submit" className="btn-signup">
-              Continue
-            </button>
+          <form className={`${styles.form} ${styles.formSignup}`}>
+            {/* ... */}
           </form>
         </div>
       </div>
--- readme.md
+++ readme.md
@@ -1,5 +1,3 @@
-fuck you
-
 tailwind
 npm install tailwindcss@latest postcss@latest autoprefixer@latest
 
