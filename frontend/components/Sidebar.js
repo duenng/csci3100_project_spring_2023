34a74f0d93e8aa586699302667219102a3dea620
TommyS725 <120657256+TommyS725@users.noreply.github.com>
2023-04-12 22:58:29 +0800

    added route to frontpage when clicking home btton and logo

--- frontend/components/Sidebar.js
+++ frontend/components/Sidebar.js
@@ -8,8 +8,17 @@ import {
   DotsCircleHorizontalIcon,
   DotsHorizontalIcon,
 } from "@heroicons/react/outline";
+import {useRouter} from "next/router";
 
 function Sidebar({ setShowProfile }) {
+
+  const router = useRouter()
+
+  const handleFront =()=>{
+      router.push("/")
+  }
+
+
   const handleProfileClick = () => {
     console.log('Profile clicked!');
     setShowProfile(true); // Call the setShowProfile function when the profile button is clicked
@@ -17,13 +26,15 @@ function Sidebar({ setShowProfile }) {
 
   return (
     <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
-      <div className="px-1.5 hoverEffect p-4 hover:bg-purple-100 xl:px-4 xl:items-start">
+      <div className="px-1.5 hoverEffect p-4 hover:bg-purple-100 xl:px-4 xl:items-start" onClick={handleFront}>
         <h1 className="text-xs font-extrabold text-purple-500 xl:text-3xl">Tertwit</h1>
       </div>
 
       {/* Menu */}
       <div className="mt-4 mb-3 xl:items-start">
+        <div onClick={handleFront}>
         <SideBarMenuItem text="Home" Icon={HomeIcon} active />
+        </div>
         <SideBarMenuItem text="Notification" Icon={BellIcon} />
         <SideBarMenuItem text="Messages" Icon={InboxIcon} />
         <SideBarMenuItem text="Profile" Icon={UserIcon} onClick={handleProfileClick} /> {/* Add an onClick handler to the profile button */}
