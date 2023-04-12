a26e780246b0b3c35de68e2103c3598130076d8b
TommyS725 <120657256+TommyS725@users.noreply.github.com>
2023-04-12 17:09:33 +0800

    fixed post

--- frontend/components/Post.jsx
+++ frontend/components/Post.jsx
@@ -1,24 +1,81 @@
-import { DotsHorizontalIcon} from "@heroicons/react/outline";
+import { ArrowLeftIcon} from "@heroicons/react/outline";
+import { useEffect, useState } from "react";
+import { useRouter } from 'next/router'
+import PostContent from "./PostContent";
+import PostCommentSection from "./PostCommentSection";
+
+
+// shd use post id to fetch
+let current = 1
+
+
+let testComment = [
+  {
+      userID:7,
+      name: "hihi",
+      tag:"@hihi",
+      avatar:null,
+      replying:"@test",
+      text:"The intricacies of language and the human mind are fascinating, as our ability to communicate complex ideas through speech and writing has shaped the course of human history, allowing us to build civilizations, share knowledge and connect with one another across time and space.",
+      images:["corgi.jpeg","doll.jpeg"],
+      video:"catVideo.mp4",
+      like:[1,2,3],
+      date:1681145476102
+  }
+]
+
+let testPost = {
+  username:"test",
+  tag:"@test",
+  avatar:null,
+  text:"here is the testing content.",
+  like:[1,3,12,4,9,17],
+  repost:[1,3,4],
+  date: new Date(2023, 3, 10, 13, 0, 20),
+  images:["corgi.jpeg","doll.jpeg","golden.png","munchkin.png","persian.png","samoyed.png","shiba.jpeg"],
+  video: "catVideo.mp4",
+  comment: testComment,
+}
+
+
+
+
 
 export default function Post(){
 
-    return(
-        <>
-        <div  className=" flex items-center justify-left text-black-700 justify-start-onlarge" >
-        {/* User Profile Picture, Need to import from database*/}
-        <img
-            src="/usericon.jpg"
-            alt="Profile Picture"
-            className=" w-12 h-12 rounded-full"
-        />
+    const [post,setPost] = useState(null)
+    const router = useRouter();
 
-        <div className="">
-        {/* User Name, Need to import from database*/}
-          <h4 style={{padding:0, margin:0}}>Username</h4>
-          <p style={{padding:0, margin:0}}>@username</p>
-        </div>
-        <DotsHorizontalIcon className="h-5 ml-auto"/>
-      </div>
-        </> 
+    useEffect(()=>{
+      return()=>{
+        setPost(testPost)
+      }
+    },[])
+      
+
+    return(
+       <>   
+            {/* header */}
+            <div className="flex text-2xl h-16 items-center bg-opacity-25 sticky top-0 backdrop-blur-sm z-10 bg-white">
+              <ArrowLeftIcon className="m-4 h-10 w-10 hoverEffect" onClick={() => router.back()}/>
+              <a className="my-4 mx-1 flex-grow">Posts</a>
+            </div>
+            {
+              post? <>
+              {/* info text media ... */}
+              <PostContent tag={post.tag} username={post.username} date={post.date} text={post.text} images={post.images} video= {post.video}/>
+              
+              {/* comment section */}
+              <PostCommentSection tag={post.tag} current={current} like={post.like} repost={post.repost} comment={post.comment}/>
+  
+              
+              {/* pop up message */}
+              {/* <PopUp message={"test"}/> */}
+              </>:null
+            }
+           
+            
+
+      </> 
     )
-}
+  }
