871c1c915c7713dcad26d9723353941194379e23
duenng <103260610+duenng@users.noreply.github.com>
2023-04-14 07:32:12 +0800

    Update index.tsx

--- frontend/pages/profile/index.tsx
+++ frontend/pages/profile/index.tsx
@@ -16,6 +16,7 @@ interface ProfileProps {
 export default function Profile({ user, token, loading, setShowProfile }: ProfileProps) {
   const router = useRouter();
   const [userData, setUserData] = useState(null);
+  const [userPosts, setUserPosts] = useState([]);
 
   console.log("rendering Profile");
 
@@ -33,6 +34,20 @@ export default function Profile({ user, token, loading, setShowProfile }: Profil
     fetchUserData();
   }, [token]);
 
+  useEffect(() => {
+    async function fetchUserPosts() {
+      try {
+        const response = await fetch(`/api/userPosts?token=${token}`);
+        const data = await response.json();
+        setUserPosts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchUserPosts();
+  }, [token]);
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -44,13 +59,35 @@ export default function Profile({ user, token, loading, setShowProfile }: Profil
 
   return (
     <>
-    <main className="flex min-h-screen max-w-7xl mx-auto "></main>
-    <div>
-      <h1>Welcome, {user.displayName}</h1>
-      <p>This is your unique page with token: {token}</p>
-      {userData && <p>User-specific data: {userData}</p>}
-      <button onClick={() => setShowProfile(false)}>Close Profile</button>
-    </div>
+      <main className="flex min-h-screen max-w-7xl mx-auto ">
+        <div className="flex-grow">
+          <h1>Welcome, {user.displayName}</h1>
+          <p>This is your unique page with token: {token}</p>
+          {userData && (
+            <div>
+              <h2>User Information</h2>
+              <p>Name: {userData.name}</p>
+              <p>Email: {userData.email}</p>
+              <p>Location: {userData.location}</p>
+            </div>
+          )}
+        </div>
+        <div className="flex-none">
+          {user.icon && (
+            <img src={user.icon} alt="User Icon" className="w-16 h-16 rounded-full" />
+          )}
+        </div>
+      </main>
+      <div>
+        <h2>Your Posts</h2>
+        {userPosts.map((post) => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        ))}
+        <button onClick={() => setShowProfile(false)}>Close Profile</button>
+      </div>
     </>
   );
 }
