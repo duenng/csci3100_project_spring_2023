4a46ad65cccafb92b214ee1e448b6cc56c92f471
TommyS725 <120657256+TommyS725@users.noreply.github.com>
2023-04-14 02:15:28 +0800

    pop up

--- frontend/components/PopUpCreatePost.jsx
+++ frontend/components/PopUpCreatePost.jsx
@@ -13,7 +13,7 @@ export default function PopUpCreate({user,url,handler}){
         <>
         <div className="fixed w-screen h-screen z-50 top-0 left-0 justify-items-center grid items-center bg-opacity-75 backdrop-blur-sm bg-slate-400" 
             onClick={(e)=>clickHandler(e)}>
-            <div className="min-w-2/5 min-h-1/4 p-2 pr-4 pt-4 bg-slate-200 rounded-xl shadow-lg shadow-violet-400">
+            <div className="w-2/5 min-h-1/4 p-2 pr-4 pt-4 bg-slate-200 rounded-xl shadow-lg shadow-violet-400">
                 <CreatePost user={user} url={url}/>
             </div>
         </div>
